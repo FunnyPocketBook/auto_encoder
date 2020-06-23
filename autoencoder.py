06e9227be31a21c84d6108c3aad6cfe42317d276
FunnyPocketBook <dang.yingkai@gmail.com>
2020-06-24 00:20:48 +0200

    Add comments and VS Code settings

--- autoencoder.py
+++ autoencoder.py
@@ -21,19 +21,21 @@ torch.set_num_threads(1) # Per default all threads
 # data loading and preprocessing
 # tbd :)
 # we want a numpy matrix with shape (?, 32, 32, 3)
+
+# Read data
 def deserialize(path):
     with open(path, 'rb') as file:
         return pickle.load(file, encoding='bytes')
 
-
+# Reshape the matrix from a (10000, 3072) matrix to a (10000, 3, 32, 32) matrix
 def reshape(matrix):
     matrix = matrix.reshape((10000, 3, 1024))
     matrix = matrix.reshape((10000, 3, 32, 32))
-    return np.moveaxis(matrix, 1, -1)
+    return np.moveaxis(matrix, 1, -1) # Move channel dimension to the last dimension
 
 def preprocess():
-    data = reshape(deserialize("cifar-10-batches-py/data_batch_1")[b'data'])
-    for i in range(2,5):
+    data = reshape(deserialize("cifar-10-batches-py/data_batch_1")[b'data']) # Load data of batch 1
+    for i in range(2,5): # Add data of batches 2-5 into data
         batch = reshape(deserialize("cifar-10-batches-py/data_batch_" + str(i))[b'data'])
         # data is in format (10000, 3072)
         data = np.append(data, batch, axis=0)
@@ -60,18 +62,23 @@ class Model(nn.Module):
         self.deconvolutional_1 = nn.ConvTranspose2d(6, 3, 11, 3)
 
     def encoder(self, x):
-        x = F.leaky_relu(self.convolutional_0(x))
-        x = F.max_pool2d(x, 4)
-        x = torch.flatten(x)
-        x = F.leaky_relu(self.linear_0(x))
+        # Change channels from 3 to 5 and apply kernel with stride
+        x = F.leaky_relu(self.convolutional_0(x)) # Shape: [1, 3, 32, 32]
+        # Reduce size of matrix by getting the maximum value of every 4x4 matrix
+        x = F.max_pool2d(x, 4) # Shape: [1, 5, 13, 13]
+        # Flatten the 4d matrix into 1d
+        x = torch.flatten(x) # Shape: [1, 5, 3, 3]
+        # Reduce vector to 10 dimensions
+        x = F.leaky_relu(self.linear_0(x)) # Shape: [45]
+        # Shape: [10]
         return x
     
     # last linear torch.sigmoid instead of leaky_relu
     def decoder(self, x):
-        x = F.leaky_relu(self.linear_1(x))
-        x = torch.reshape(x, (1, 4, 3, 3))
-        x = F.leaky_relu(self.deconvolutional_0(x))
-        x = F.leaky_relu(self.deconvolutional_1(x))
+        x = F.leaky_relu(self.linear_1(x)) # Shape: [10]
+        x = torch.reshape(x, (1, 4, 3, 3)) # Shape: [36]
+        x = F.leaky_relu(self.deconvolutional_0(x)) # Shape: [1, 6, 8, 8]
+        x = F.leaky_relu(self.deconvolutional_1(x)) # Shape: [1, 3, 32, 32]
         return x
     
     def forward(self, x):
@@ -79,6 +86,7 @@ class Model(nn.Module):
         x = self.decoder(x)
         return x
 
+# Shows input and output image
 def show_image(original, trained):
     fig, axes = plt.subplots(2, 1)
     axes[0].imshow(original)
@@ -86,10 +94,13 @@ def show_image(original, trained):
     plt.tight_layout()
     plt.show()
     plt.clf()
-    plt.close()    
+    plt.close()
 
+# Actually training the data
 def training(model, training_data):
+    # optimizer
     optimizer = optim.Adam(model.parameters(), lr=0.001)
+    # Function for getting the mean square error loss
     criterion = nn.MSELoss()
 
     iterations = 0
@@ -100,20 +111,26 @@ def training(model, training_data):
     while (epoch < max_epoch):
         for idx, elem in enumerate(training_data):
             iterations += 1
+            # Create tensor from matrix
             tensor = torch.from_numpy(elem).to(device)
             # has shape (32, 32, 3)
             # we want (1, 3, 32, 32)
+            # Change dimension positions
             target = tensor.permute(2, 0, 1).unsqueeze(0)
+            # Reset optimizer for each data
             optimizer.zero_grad()
             output = model(target)
             loss = criterion(output, target)
+            # Track loss
             running_loss += loss.item()
             print("loss: {0: .4f}, epoch: {1}, iterations: {2}"
                 .format(running_loss / iterations, epoch, iterations))
+            # Backtracking
             loss.backward()
             optimizer.step()
         epoch += 1
     
+    # Reconstructing original image for showing it
     original = training_data[0]
     model.eval()
     reconstructed = torch.tensor(elem, requires_grad=True).to(device)
@@ -126,4 +143,4 @@ def training(model, training_data):
 a = preprocess()
 auto_encoder = Model(32, 32, 3)
 auto_encoder.cuda(device)
-training(auto_encoder, a[0:10])
+training(auto_encoder, a[0:1])

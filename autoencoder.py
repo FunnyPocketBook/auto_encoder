07e976a341293ba1e8131da2b0f15daefb6882e5
FunnyPocketBook <dang.yingkai@gmail.com>
2020-07-01 04:10:58 +0200

    repair

--- autoencoder.py
+++ autoencoder.py
@@ -20,12 +20,6 @@ loss_function = "mse"
 # https://ml-cheatsheet.readthedocs.io/en/latest/layers.html
 # https://ml-cheatsheet.readthedocs.io/en/latest/nn_concepts.html
 # https://ruder.io/optimizing-gradient-descent/index.html
-# extend network such that on 10 images the output is correct. Then to 100
-# at the beginning, only do up to 1000 bc if that's good, 50k are also going to be good
-# if the network is grey or black and stuck, as the god
-# data loading and preprocessing
-# tbd :)
-# we want a numpy matrix with shape (?, 32, 32, 3)
 
 # Read data
 def deserialize(path):
@@ -40,12 +34,61 @@ def reshape(matrix):
 
 def preprocess():
     data = reshape(deserialize("cifar-10-batches-py/data_batch_1")[b'data']) # Load data of batch 1
-    for i in range(2,5): # Add data of batches 2-5 into data
+    for i in range(2,6): # Add data of batches 2-5 into data
         batch = reshape(deserialize("cifar-10-batches-py/data_batch_" + str(i))[b'data'])
         # data is in format (10000, 3072)
         data = np.append(data, batch, axis=0)
-    
     return np.float32(data / 255)
+
+def load_labels():
+    labels = deserialize("cifar-10-batches-py/data_batch_1")[b'labels'] # Load data of batch 1
+    for i in range(2,6): # Add data of batches 2-5 into data
+        batch = deserialize("cifar-10-batches-py/data_batch_" + str(i))[b'labels']
+        # data is in format (10000, 3072)
+        labels = np.append(labels, batch, axis=0)
+    test_labels = deserialize("cifar-10-batches-py/test_batch")[b'labels']
+    return labels, test_labels
+
+def load_test_batch():
+    return np.float32(reshape(deserialize("cifar-10-batches-py/test_batch")[b'data']) / 255)
+
+class RingBuffer():
+    """
+   I stole this from Samuel Schiegg (with permission)
+   """
+    def __init__(self, max_length, reactivity_factor=10):
+        self.max_length = max_length
+        self.index = 0
+        self.rb = []
+        self.intermed = []
+        self.reactivity_factor = reactivity_factor
+ 
+    def add(self, x):
+        if len(self.rb) < self.max_length:
+            self.rb.append(x)
+            self.index += 1
+        else:
+            if len(self.intermed) < self.__stability():
+                self.intermed.append(x)
+            else:
+                x = sum(self.intermed) / len(self.intermed)
+                if self.index >= self.max_length:
+                    self.index = 0
+                self.rb[self.index] = x
+                self.index += 1        
+                self.intermed.clear()
+ 
+    def __stability(self):
+        if self.reactivity_factor > self.max_length:
+            self.reactivity_factor = self.max_length
+        return (int)(self.max_length / self.reactivity_factor)
+ 
+    def clear(self):
+        self.rb = []
+        self.index = 0
+ 
+    def avg(self):
+        return sum(self.rb) / len(self.rb)
 
 
 # pytorch model must always define all layers in constructor
@@ -58,18 +101,18 @@ class Model(nn.Module):
 
         # encode stage
         # input channels, output channels, kernel (2,2), maybe stride (2,2)
-        self.convolutional_0 = nn.Conv2d(3, 13, 1, 1)
-        self.convolutional_1 = nn.Conv2d(13, 26, 3, 2)
+        self.convolutional_0 = nn.Conv2d(3, 13, 1)
+        self.convolutional_1 = nn.Conv2d(13, 26, 2, 2)
         self.convolutional_2 = nn.Conv2d(26, 38, 2, 2)
-        self.convolutional_3 = nn.Conv2d(38, 44, 2, 2)
-        self.linear_0 = nn.Linear(396, 10)
+        self.convolutional_3 = nn.Conv2d(38, 45, 2, 3)
+        self.linear_0 = nn.Linear(405, 10)
 
         # decode stage
         self.linear_1 = nn.Linear(10, 16)
-        self.deconvolutional_0 = nn.ConvTranspose2d(1, 3, 2)
-        self.deconvolutional_1 = nn.ConvTranspose2d(3, 6, 2, 2)
-        self.deconvolutional_2 = nn.ConvTranspose2d(6, 9, 3, 2)
-        self.deconvolutional_3 = nn.ConvTranspose2d(9, 10, 2)
+        self.deconvolutional_0 = nn.ConvTranspose2d(1, 5, 2, 1)
+        self.deconvolutional_1 = nn.ConvTranspose2d(5, 9, 2, 2)
+        self.deconvolutional_2 = nn.ConvTranspose2d(9, 14, 3, 2)
+        self.deconvolutional_3 = nn.ConvTranspose2d(14, 19, 2, 1)
         self.linear_2 = nn.Linear(self.get_decode_lin_shape(), 3072)
 
     def get_decode_lin_shape(self):
@@ -80,20 +123,14 @@ class Model(nn.Module):
     def encoder(self, x):
         # Change channels from 3 to 5 and apply kernel with stride
         x = F.leaky_relu(self.convolutional_0(x)) # Shape: [1, 13, 32, 32]
-        x = F.leaky_relu(self.convolutional_1(x)) # Shape: [1, 26, 16, 16]
+        x = F.leaky_relu(self.convolutional_1(x)) # Shape: [1, 26, 26, 26]
         x = F.leaky_relu(self.convolutional_2(x)) # Shape: [1, 38, 8, 8]
-        x = F.leaky_relu(self.convolutional_3(x)) # Shape: [1, 44, 4, 4]
-        # Reduce size of matrix by getting the maximum value of every 4x4 matrix
-        # x = F.max_pool2d(x, 4) # Shape: [1, 10, 7, 7]
-        # Flatten the 4d matrix into 1d
+        x = F.leaky_relu(self.convolutional_3(x)) # Shape: [1, 45, 3, 3]
         x = torch.flatten(x) # Shape: [48]
-        # Reduce vector to 10 dimensions
         x = F.leaky_relu(self.linear_0(x)) # Shape: [10]
         return x
     
-    # last linear torch.sigmoid instead of leaky_relu
     def decoder(self, x):
-        #x = torch.reshape(x, (1, 1, 1, 10))
         x = F.leaky_relu(self.linear_1(x)) # Shape: [16]
         x = torch.reshape(x, (1, 1, 4, 4))
         x = self.decode_conv(x)
@@ -102,10 +139,10 @@ class Model(nn.Module):
         return x
 
     def decode_conv(self, x):
-        x = F.leaky_relu(self.deconvolutional_0(x)) # Shape: [1, 3, 5, 5]
-        x = F.leaky_relu(self.deconvolutional_1(x)) # Shape: [1, 5, 10, 10]
-        x = F.leaky_relu(self.deconvolutional_2(x)) # Shape: [1, 7, 21, 21]
-        x = F.leaky_relu(self.deconvolutional_3(x)) # Shape: [1, 9, 22, 22]
+        x = F.leaky_relu(self.deconvolutional_0(x)) # Shape: [1, 5, 5, 5]
+        x = F.leaky_relu(self.deconvolutional_1(x)) # Shape: [1, 9, 10, 10]
+        x = F.leaky_relu(self.deconvolutional_2(x)) # Shape: [1, 14, 21, 21]
+        x = F.leaky_relu(self.deconvolutional_3(x)) # Shape: [1, 19, 22, 22]
         x = torch.flatten(x)
         return x
 
@@ -115,13 +152,12 @@ class Model(nn.Module):
         return x
 
 # Shows input and output image
-def show_image(model, training_data, epoch, loss, n, start_time, model_name, save=False):
+def show_image(model, training_data, epoch, loss, n, model_name, save=False):
     model.eval()
     fig, axes = plt.subplots(2, len(training_data))
     for i in range(0, len(training_data)):
         original = training_data[i]
         elem = training_data[i]
-        fig.suptitle("Comparison, loss: {0:.6f}".format(loss))
         tensor = torch.from_numpy(elem).to(device)
         trained = torch.tensor(elem, requires_grad=True).to(device)
         trained = tensor.permute(2, 0, 1).unsqueeze(0)
@@ -144,9 +180,18 @@ def show_image(model, training_data, epoch, loss, n, start_time, model_name, sav
     plt.clf()
     plt.close()
 
+def show_loss(model_name, history_loss, n, epoch, save=False):
+    plt.plot(range(0,len(history_loss)), history_loss)
+    if save:
+        directory = "result/{0}".format(model_name)
+        plt.savefig("{2}/loss_ep{0}_n{1}.png".format(epoch, n, directory))
+    else:
+        plt.show()
+    plt.clf()
+    plt.close()
+
 
 def save_model(path, epoch, iterations, model, optimizer, loss, history_loss):
-    print("\nSaving model...")
     torch.save({
         "epoch": epoch,
         "iterations": iterations,
@@ -155,22 +200,10 @@ def save_model(path, epoch, iterations, model, optimizer, loss, history_loss):
         "loss": loss,
         "history_loss": history_loss
     }, path+".pt")
-    print("Model saved")
-    if epoch % 50 == 0:
-        print("Creating backup...")
-        torch.save({
-            "epoch": epoch,
-            "iterations": iterations,
-            "model_state_dict": model.state_dict(),
-            "optimizer_state_dict": optimizer.state_dict(),
-            "loss": loss,
-            "history_loss": history_loss
-        }, path+"_e{0}.pt".format(epoch))
-        print("Backup created")
 
 
 # Actually training the data
-def training(model, training_data):
+def training(model, training_data, test_data):
     # optimizer
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     # Function for getting the mean square error loss
@@ -183,13 +216,14 @@ def training(model, training_data):
     elif loss_function == "bce":
         criterion = nn.BCELoss()
 
+    ring_buffer = RingBuffer(1000)
     iterations = 0
     running_loss = 0
     history_loss = []
     epoch = 0
-    start_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     model_note = ""
     model_name = "model_{0}_lr{1}_n{2}{3}".format(loss_function, learning_rate, len(training_data), model_note)
+    #model_name = "model_mse_lr0.0005_n50000"
     model_path = "model/{0}".format(model_name)
 
     try:
@@ -204,10 +238,13 @@ def training(model, training_data):
     except FileNotFoundError:
         print("no model found")
 
+    start_time = datetime.datetime.now()
+    progress = ""
     try:
         while True:
             for idx, elem in enumerate(training_data):
                 iterations += 1
+                time = datetime.datetime.now()
                 # Create tensor from matrix
                 tensor = torch.from_numpy(elem).to(device)
                 # Change dimension positions
@@ -216,25 +253,37 @@ def training(model, training_data):
                 optimizer.zero_grad()
                 output = model(target)
                 loss = criterion(output, target)
-                history_loss.append(loss)
                 # Track loss
                 running_loss += loss.item()
-                print("\rloss: {0: .6f}, epoch: {1}, iterations: {2}"
-                    .format(running_loss / iterations, epoch, iterations), end='')
+                loss_current = running_loss / iterations
+                history_loss.append(loss_current)
                 if iterations % 1000 == 0:
-                    show_image(model, training_data[20:30], epoch, running_loss/iterations, len(training_data), start_time, model_name, save=True)
+                    show_image(model, test_data[0:10], epoch, running_loss/iterations, len(training_data), model_name, save=True)
                 # Backtracking
                 loss.backward()
                 optimizer.step()
+                time = datetime.datetime.now() - time
+                ring_buffer.add(time.microseconds)
+                elapsed = datetime.datetime.now() - start_time
+                elapsed = elapsed - datetime.timedelta(microseconds=elapsed.microseconds)
+                remaining = datetime.timedelta(microseconds=ring_buffer.avg() * (len(training_data) - idx))
+                remaining = remaining - datetime.timedelta(microseconds=remaining.microseconds)
+                if iterations % 50 == 0:
+                    progress = "\rTraining on n={0}: loss: {6:.6f} | epoch: {5: >3} | {1: >6}/{0} | {2: >4}ms/iteration | elapsed: {3:>8} | remaining: {4:>8}" \
+                        .format(len(training_data),
+                            iterations%len(training_data),
+                            int(ring_buffer.avg()/1000),
+                            str(elapsed),
+                            str(remaining), 
+                            epoch, 
+                            running_loss/iterations)
+                    print(progress, end="")
             epoch += 1
             save_model(model_path, epoch, iterations, model, optimizer, running_loss, history_loss)
-        show_image(model, training_data[20:30], epoch, running_loss/iterations, len(training_data), start_time, model_name, save=True)
+            print("")
+        show_image(model, test_data[0:10], epoch, running_loss/iterations, len(training_data), model_name, save=True)
     except KeyboardInterrupt:
-        print("Keyboard interrupt")
-        show_image(model, training_data[20:30], epoch, running_loss/iterations, len(training_data), start_time, model_name, save=True)
+        print("\nKeyboard interrupt")
+        #show_image(model, test_data[0:10], epoch, running_loss/iterations, len(training_data), model_name, save=True)
         save_model(model_path, epoch, iterations, model, optimizer, running_loss, history_loss)
-
-a = preprocess()
-auto_encoder = Model(32, 32, 3)
-auto_encoder.cuda(device)
-training(auto_encoder, a[0:1000])
+    show_loss(model_name, history_loss, len(training_data), epoch, save=True)
